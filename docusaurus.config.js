f3d12ede5ad95f9a734a88504ef7e75cb54a5e2b
Cristiano Rosa <cristiano.rosa@acin.pt>
2024-05-29 16:07:16 +0100

    chore: docusaurus - update deploymentBranch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'paypayue', // Usually your GitHub org/user name.
   projectName: 'paypayue.github.io', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'deployment',
   trailingSlash: false,
 
   onBrokenLinks: 'ignore',
