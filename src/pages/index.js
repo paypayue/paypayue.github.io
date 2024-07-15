8ab21bbf945c266fd7ceb2df5ce4d4250f3923a8
Cristiano Rosa <cristiano.rosa@acin.pt>
2024-07-15 16:57:15 +0100

    chore: homepage - update according to design prototype

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,29 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Translate from '@docusaurus/Translate';
-import Logo from '@site/static/img/logo-full.svg'
 import styles from './index.module.css';
 
 function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <div style={{width: '200px', margin: '0 auto'}}>
-          <Logo />
+      <div className='container'>
+        <div className='row'>
+          <div className='col col--6'>
+            <p className='hero__subtitle hero__subtitle--primary'>
+              <Translate id="homepage.headerDescription">
+                Bem vindo!
+              </Translate>
+            </p>
+            <p className='hero__subtitle hero__subtitle--secondary'>Aqui pode aceder à área de desenvolvimento do PayPay</p>
+          </div>
+          <div className='col col--6'>
+            <div className='hero__logo'></div>
+          </div>
         </div>
-        <p className="hero__subtitle">
-          <Translate id="homepage.headerDescription">
-            A maneira mais simples de gerir pagamentos
-          </Translate>
-        </p>
       </div>
     </header>
+
+    // ver integracao link hidden
   );
 }
 
