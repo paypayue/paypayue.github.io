d875947556dd91007ded9c5bef1d0901668ff415
Cristiano Rosa <cristiano.rosa@acin.pt>
2024-07-17 11:32:24 +0100

    chore: docusaurus config - remove test url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'http://10.11.16.37/',//site 'https://paypayue-dev.github.io'
+  url: site,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: baseUrl,
