d5e7dc04c5be12bdc6ea9be22683175fe312ee83
Cristiano Rosa <cristiano.rosa@acin.pt>
2024-07-25 10:18:58 +0100

    chore: rework clickShowIframe and update on each mdx

--- src/utils/ClickShowIframe.js
+++ src/utils/ClickShowIframe.js
@@ -1,8 +1,6 @@
 import OpenIframe from "./OpenIframe";
 
-const ClickShowIframe = (typeMenu = 'tag', idMenuOption, idOption = null) => {
-    console.log(typeMenu);
-
+const ClickShowIframe = ({typeMenu = 'tag', idMenuOption, idOption = null}) => {
     let menuOptionType = (typeMenu !== 'model') ? 'sidebar-tag/' : 'sidebar-models';
     let menuOptionHash = (typeMenu !== 'model') ? '#tag/' : '#model/';
 
@@ -10,7 +8,7 @@ const ClickShowIframe = (typeMenu = 'tag', idMenuOption, idOption = null) => {
     let firstTargetHash = menuOptionHash + idMenuOption;
 
     let secondTargetId =(typeMenu !== 'model') ? menuOptionType + idOption : menuOptionType;
-    let secondTargetHash = menuOptionHash + idMenuOption
+    let secondTargetHash = menuOptionHash + idMenuOption;
 
     let iframePanel = document.getElementById('iframePanel');
     let previousOptionIsOpen = iframePanel.contentDocument.querySelectorAll('li.sidebar-group-item ul');
