06b2271429b19266af3fe83e6440346a30b509e0
Cristiano Rosa <cristiano.rosa@acin.pt>
2024-08-08 16:33:19 +0100

    chore: docusaurus - update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -134,7 +134,7 @@ const config = {
       (
         {
           id: 'scalar2',
-          label: 'EspecificaçãoES',
+          label: 'Nada0',
           route: '/es' + api,
           configuration: {
             spec: {
@@ -150,7 +150,7 @@ const config = {
       (
         {
           id: 'scalar3',
-          label: 'EspecificaçãoEN',
+          label: 'Nada1',
           route: '/en' + api,
           configuration: {
             spec: {
