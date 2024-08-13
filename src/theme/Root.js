288e60e8c102168ace8af7445ac4f80efbb82599
Cristiano Rosa <cristiano.rosa@acin.pt>
2024-08-13 17:19:02 +0100

    chore: docusaurus - update js to add active class to Specification only on its page

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -51,6 +51,20 @@ export default function Root({children}) {
             </>
         );
 
+    } else if (locationMatchApi) {
+        console.log('IFapi');
+        useEffect(() => {
+            console.log('USEEFECTapi')
+            window.setTimeout(() => {
+                document.querySelector('.active-on-selected').classList.add('navbar__link--active');
+            }, 850);
+        }, []);
+
+        return (
+            <>
+                {children}
+            </>
+        );
     } else {
 
         if (locationMatchApi) {
