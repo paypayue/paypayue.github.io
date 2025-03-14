d7f96a8ffd9be281bf4e8aca3ada3599e93e3870
Luís Gonçalves <apoio@paypay.pt>
2025-03-14 15:31:22 +0000

    feat: Add translation files and update localization for PrestaShop integration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: site,
+  url: 'http://localhost',//site,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: baseUrl,
