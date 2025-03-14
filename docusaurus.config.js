c3ddd2b44335c5e899b5b238cac6ee17adaceeef
Luís Gonçalves <apoio@paypay.pt>
2025-03-14 15:32:54 +0000

    fix: Update site URL configuration in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'http://localhost',//site,
+  url: site,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: baseUrl,
